096925ab8ef10b7ff3fa2671aa0df07472e63400
Pradeep Felix <fpradeepjoe@gmail.com [merge] tool = tortoisePF [mergetool tortoisePF] cmd = cmd = "c:/Program Files/TortoiseGIT/bin/TortoiseGitMerge.exe" -mine "$REMOTE" -base "$LOCAL>
2016-05-18 22:50:36 +0530

    new hello world project

---
+++ MyHelloWorldTest/ProgramTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyHelloWorld;
+
+
+namespace MyHelloWorldTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World", Program.SayHello());
+        }
+    }
+}
